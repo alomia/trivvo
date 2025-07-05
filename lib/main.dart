2cfd9bad80f3341c49487b21c759d40d04d52614
Alejandro Alomia <alejandroalomia730@gmail.com>
2025-07-05 01:52:35 -0500

    feat: environment variables configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:trivvo/config/router/app_router.dart';
 import 'package:trivvo/config/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+
   runApp(const MainApp());
 }
 
