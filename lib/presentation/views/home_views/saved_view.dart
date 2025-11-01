3b927728f128b1b6c7c19424f6a2038b20ec5ab0
Alejandro Alomia <alejandroalomia730@gmail.com>
2025-11-01 02:24:53 -0500

    feat(saved-view): show message when there are no saved movies

--- lib/presentation/views/home_views/saved_view.dart
+++ lib/presentation/views/home_views/saved_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:trivvo/presentation/providers/providers.dart';
 import 'package:trivvo/presentation/widgets/widgets.dart';
 
@@ -8,6 +9,8 @@ class SavedView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final textTheme = Theme.of(context).textTheme;
+
     final favoriteMovies = ref
         .watch(favoriteMoviesNotifierProvider)
         .values
@@ -15,24 +18,43 @@ class SavedView extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text("Guardado")),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            mainAxisSpacing: 10.0,
-            childAspectRatio: 0.666,
-          ),
-          itemCount: favoriteMovies.length,
-          itemBuilder: (context, index) {
-            final movie = favoriteMovies[index];
-            return MoviePoster(
-              imageUrl: movie.posterPath,
-              navigationRoute: '/saved/movie/${movie.id}',
-            );
-          },
-        ),
-      ),
+      body: favoriteMovies.isNotEmpty
+          ? Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 10.0,
+                  childAspectRatio: 0.666,
+                ),
+                itemCount: favoriteMovies.length,
+                itemBuilder: (context, index) {
+                  final movie = favoriteMovies[index];
+                  return MoviePoster(
+                    imageUrl: movie.posterPath,
+                    navigationRoute: '/saved/movie/${movie.id}',
+                  );
+                },
+              ),
+            )
+          : SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  Icon(PhosphorIconsRegular.bookmarkSimple, size: 60.0),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'Aún no tienes películas guardadas',
+                    style: textTheme.titleMedium!.copyWith(
+                      fontWeight: FontWeight.w400,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
